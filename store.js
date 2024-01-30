de98bf1a94e2820478e527effad7476de9a7e1db
Geralyn Chong <136625723+asprgchong@users.noreply.github.com>
2024-01-30 08:12:51 -0600

    Update store.js

--- store.js
+++ store.js
@@ -1,1 +1,17 @@
+const store_info = ` [
+    {
+      "name": "51st S. Ellis",
+      "description": "3 Bedroom, 2 Bath at $3000/month"
+    }, 
+    {
+        "name": "1434-1400 E 56th St",
+        "description": "6 Bedroom, 3 Bath at $2500/month"
+    },
+    {
+        "name": "S Dorchester Ave",
+        "description": "1 Bedroom, 1 Bath at $1800/month"
+    }
+  ]
+`
+  export default store_info;
 
